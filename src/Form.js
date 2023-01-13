9533a2df2cd562a299febff65db259c535ef2153
yang, anna <annayang877@gmail.com>
2023-01-13 14:40:30 -0800

    added submit button to update form

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState(
         {
             name: "",
@@ -10,7 +10,7 @@ function Form() {
 
     function handleChange(event) {
         const { name, value } = event.target;
-        if (name == "job")
+        if (name === "job")
             setPerson(
                 {name: person['name'], job: value}
             );
@@ -18,6 +18,11 @@ function Form() {
             setPerson(
                 {name: value, job: person['job']}
             );
+    }
+
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
     }
 
     return (
@@ -36,6 +41,7 @@ function Form() {
                 id="job"
                 value={person.job}
                 onChange={handleChange} />
+            <input type="button" value= "Submit" onClick={submitForm} />
         </form>
     );
 }
