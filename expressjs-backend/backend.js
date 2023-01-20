aa499b5332371e367d6e73a6e38e66b0cb6cbe48
yang, anna <annayang877@gmail.com>
2023-01-20 13:48:52 -0800

    added delete and filter by name and job operations

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -10,6 +10,11 @@ const users = {
           name : 'Charlie',
           job: 'Janitor',
        },
+       { 
+         id : 'xyz789',
+         name : 'Charlie',
+         job: 'Professor',
+       },
        {
           id : 'abc123', 
           name: 'Mac',
@@ -29,7 +34,12 @@ const users = {
           id: 'zap555', 
           name: 'Dennis',
           job: 'Bartender',
-       }
+       },
+       {
+         "id": "qwe123",
+         "job": "Zookeeper",
+         "name": "Cindy"
+      }
     ]
  }
 
@@ -73,4 +83,47 @@ app.get('/users/:id', (req, res) => {
 function findUserById(id) {
    return users['users_list'].find( (user) => user['id'] === id); // or line below
    // return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.toString('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.status(200).end();
+});
+
+function addUser(user){
+   users['users_list'].push(user);
+}
+
+// remove a particular user by id
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id']; // or req.params.id
+   let result = filterOutUserById(id);
+   if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+   else {
+      result = {users_list: result};
+      res.send(result);
+   }
+});
+
+function filterOutUserById(id) {
+   return users['users_list'].filter( (user) => user['id'] != id);
+}
+
+// get all users that match a given name and job
+app.get('/users/:name/:job', (req, res) => {
+   const name = req.params['name']; // or req.params.id
+   const job = req.params['job'];
+   let result = findUserByNameJob(name, job);
+   if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+   else {
+      result = {users_list: result};
+      res.send(result);
+   }
+});
+
+function findUserByNameJob(name, job) {
+   return users['users_list'].find( (user) => user['name'] === name && user['job'] === job);
 }
