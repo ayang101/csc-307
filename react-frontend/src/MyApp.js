d494177bf85e5fbefc3a208ba2d46ce4a67e79f6
yang, anna <annayang877@gmail.com>
2023-01-23 00:13:22 -0800

    POST request to add new users

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -23,7 +23,7 @@ function MyApp() {
 
   function updateList(person) {
     makePostCall(person).then( result => {
-    if (result && result.status === 201)
+    if (result && result.status === 200)
       setCharacters([...characters, person]);
     });
   }
